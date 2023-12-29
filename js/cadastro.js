132ab6f80f99720e6e369d78779ac9b38962d43b
Davi Bittencourt <dbittencourt2002@gmail.com>
2023-12-29 10:51:47 -0300

    adição de ícone favorito

--- js/cadastro.js
+++ js/cadastro.js
@@ -2,12 +2,14 @@ import { adicionarHabilidadeNaLista } from "./habilidades.js";
 import { obterFotoParticipante, ampliarFoto } from "./fotoParticipante.js";
 import { redimensionarCaixaTexto } from "./caixaTexto.js";
 
+const iconeFavorito = document.getElementById("icone-favorito");
 const campoAdicaoHabilidade = document.getElementById("nova-habilidade");
 const botaoAdicaoHabilidade = campoAdicaoHabilidade.querySelector("button");
 const inputFotoParticipante = document.getElementById("input-foto-participante");
 const fotoParticipante = document.getElementById("foto-participante");
 const areasTexto = Array.from(document.querySelectorAll("textarea"));
 
+iconeFavorito.addEventListener("click", () => iconeFavorito.classList.toggle("favoritado"));
 botaoAdicaoHabilidade.addEventListener("click", adicionarHabilidadeNaLista);
 fotoParticipante.addEventListener("click", ampliarFoto);
 inputFotoParticipante.addEventListener("change", obterFotoParticipante);
